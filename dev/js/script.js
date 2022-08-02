2b878c7dfb7896a61bbc520b7b31a513b44dec6d
IsaevEgor <isaevegor.mail@gmail.com>
2022-08-02 15:14:46 +0300

    fix form

--- dev/js/script.js
+++ dev/js/script.js
@@ -75,9 +75,12 @@ document.addEventListener('DOMContentLoaded', function () {
 		let formData = new FormData(form);
 		formData.append('image', formFile.files[0]);
 
-		if (error === undefined) {
+		/*if (error === undefined) {
 			form.classList.add('_sending')
-
+			let responce = await fetch('sendmail.php', {
+				method: 'POST',
+				body: formData
+			});
 			if (responce.ok) {
 				let result = await responce.json();
 				alert(result.message);
@@ -91,6 +94,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		} else {
 			alert ('заполните форму')
 		}
+		*/
 	}
 
 	function formValidate(form) {
@@ -158,5 +162,55 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 /*
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->IsHTML(true);
+
+$mail->setFrom('egorisaev.work@mail.ru', 'Egor Isaev');
+$mail->addAddress('isaevegor.work@gmail.com');
+$mail->Subject = 'Письмо из портфолио';
+
+$body = '<h1>Новое письмо</h1>';
+
+if(trim(!empty($_POST['name']))){
+	$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+}
+if(trim(!empty($_POST['email']))){
+	$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+}
+if(trim(!empty($_POST['message']))){
+	$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+}
+
+if(!empty($_FILES['image']['tmp_name'])) {
+	$filePath = __DIR__ . '/files/' . $_FILES['image']['name'];
+
+	if(copy($_FILES['images']['tmp_name'], $filePath)){
+		$fileAttach = $filePath;
+		$body.='<p><strong>Файлы:</strong>.</p>';
+		$mail->addAttachment($fileAttach);
+	}
+}
+
+$mail->Body = $body;
+
+if (!$mail->send()) {
+	$message = 'Ошибка php';
+} else {
+	$message = 'Данные отправлены';
+}
+
+$response = ['message' => $message];
+
+header('Content-type: application/json');
+echo json_encode($response);
+?>
 */
